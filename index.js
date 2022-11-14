40fcfdc35068283d9279cd22f16d0ab3a5793bf5
FrancoRodr20998 <117591789+FrancoRodr20998@users.noreply.github.com>
2022-11-13 21:09:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const https = require('https');
-const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,8 +9,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, "public")))
 
 app.listen(PORT, () => console.log("Server is listening on port " + PORT))
-http.createServer(app).listen(80);
-https.createServer(options, app).listen(443);
 
 app.get('/', function(req, res, next) {
     res.send("Hello world");
